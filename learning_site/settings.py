2f588152bd5eae9c63d71b4ac09716e40df6000a
Patrick <patrick.bluth@gmail.com>
2018-01-27 21:20:32 -0500

    Updated CSS and Layouts

--- learning_site/settings.py
+++ learning_site/settings.py
@@ -120,7 +120,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/css/'
+
+STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'assets'),
 )
