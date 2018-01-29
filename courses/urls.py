7f38e153bcd72bd76ccfbd5650dfd040613ad470
Patrick <patrick.bluth@gmail.com>
2018-01-29 14:34:05 -0500

    Seperated steps templates into quiz and text templates

--- courses/urls.py
+++ courses/urls.py
@@ -7,7 +7,7 @@ from . import views
 app_name = 'courses'
 urlpatterns = [
     path('', views.course_list, name='course_list'),
-    path('<int:course_pk>/<int:step_pk>/', views.text_detail, name='text'),
-    path('<int:course_pk>/<int:step_pk>/', views.quiz_detail, name='quiz'),
+    path('<int:course_pk>/t<int:step_pk>/', views.text_detail, name='text'),
+    path('<int:course_pk>/q<int:step_pk>/', views.quiz_detail, name='quiz'),
     path('<int:pk>/', views.course_detail, name='detail'),
 ]
