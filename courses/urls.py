9d6f8d9c020dd3fc73cb67779231ec655c2a7c24
Patrick <patrick.bluth@gmail.com>
2018-01-29 18:30:44 -0500

    added cms to create and edit quizes

--- courses/urls.py
+++ courses/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path('', views.course_list, name='course_list'),
     path('<int:course_pk>/t<int:step_pk>/', views.text_detail, name='text'),
     path('<int:course_pk>/q<int:step_pk>/', views.quiz_detail, name='quiz'),
+    path('<int:course_pk>/create_quiz/', views.quiz_create, name='quiz_create'),
+    path('<int:course_pk>/edit_quiz/<int:quiz_pk>/', views.quiz_edit, name='quiz_edit'),
     path('<int:pk>/', views.course_detail, name='detail'),
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,8 +1,11 @@
 from itertools import chain
 
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 
-from . import models
+from . import models, forms
 
 def course_list(request):
     courses = models.Course.objects.all()
@@ -24,6 +27,37 @@ def text_detail(request, course_pk, step_pk):
     step = get_object_or_404(models.Text, course_id=course_pk, pk=step_pk)
     return render(request, 'courses/text_detail.html', {'step': step})
 
+
 def quiz_detail(request, course_pk, step_pk):
     step = get_object_or_404(models.Quiz, course_id=course_pk, pk=step_pk)
     return render(request, 'courses/quiz_detail.html', {'step': step})
+
+@login_required
+def quiz_create(request, course_pk):
+    course = get_object_or_404(models.Course, pk=course_pk)
+    form = forms.QuizForm()
+
+    if request.method == 'POST':
+        form = forms.QuizForm(request.POST)
+        if form.is_valid():
+            quiz = form.save(commit=False)
+            quiz.course = course
+            quiz.save()
+            messages.add_message(request, messages.SUCCESS,
+                                 'Quiz added!')
+            return HttpResponseRedirect(quiz.get_absolute_url())
+    return render(request, 'courses/quiz_form.html', {'form': form, 'course': course})
+
+@login_required
+def quiz_edit(request, course_pk, quiz_pk):
+    quiz = get_object_or_404(models.Quiz, pk=quiz_pk, course_id=course_pk)
+    form = forms.QuizForm(instance=quiz)
+
+    if request.method == 'POST':
+        form = forms.QuizForm(instance=quiz, data=request.POST)
+        if form.is_valid():
+            quiz = form.save(commit=False)
+            form.save()
+            messages.success(request, "Updated {}".format(form.cleaned_data['title']))
+            return HttpResponseRedirect(quiz.get_absolute_url())
+    return render(request, 'courses/quiz_form.html', {'form': form, 'course': quiz.course})
