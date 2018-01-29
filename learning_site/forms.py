6cd7f23e0c9bb02454e4b10e162d1bba8addd2bd
Patrick <patrick.bluth@gmail.com>
2018-01-28 21:47:47 -0500

    added verify email field to suggestion form

--- learning_site/forms.py
+++ learning_site/forms.py
@@ -8,8 +8,16 @@ def must_be_empty(value):
 class SuggestionForm(forms.Form):
     name = forms.CharField()
     email = forms.EmailField()
+    verify_email = forms.EmailField(label='Verify Email')
     suggestion = forms.CharField(widget=forms.Textarea)
     honeypot = forms.CharField(required=False,
                                widget=forms.HiddenInput,
                                label="Leave empty",
                                validators=[must_be_empty],)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        email = cleaned_data.get('email')
+        verify = cleaned_data.get('verify_email')
+        if email != verify:
+            raise forms.ValidationError('Emails did not match')
