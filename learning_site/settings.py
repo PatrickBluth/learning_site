d9c689fd31a483e33b0a30ced2fe98e4aa76f253
Patrick <patrick.bluth@gmail.com>
2018-01-31 17:23:29 -0500

    Configured django debug toolbar

--- learning_site/settings.py
+++ learning_site/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
+    'debug_toolbar',
     'courses',
 
 ]
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'learning_site.urls'
@@ -128,3 +130,9 @@ STATICFILES_DIRS = (
 
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'suggestions')
+
+def show_toolbar(request):
+    return True
+DEBUG_TOOLBAR_CONFIG = {
+    "SHOW_TOOLBAR_CALLBACK" : show_toolbar,
+}
--- learning_site/urls.py
+++ learning_site/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import debug_toolbar
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path
@@ -25,7 +26,8 @@ urlpatterns = [
     path('courses/', include('courses.urls'), name='list'),
     path('admin/', admin.site.urls),
     path('', views.homepage, name='homepage'),
-    path('suggest/', views.suggestion_view, name='suggestion')
+    path('suggest/', views.suggestion_view, name='suggestion'),
+    path(r'^__debug__/', include(debug_toolbar.urls)),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
